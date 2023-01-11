aad6c435da2cd9f9d40aae5fa3d8f8858196d355
Alex Dauncey <alexanderdauncey@gmail.com>
2023-01-11 11:27:40 +0000

    llvm test proj, shellscript for cmake

---
+++ Iridium/Codegen/include/Codegen/Codegen.h
@@ -0,0 +1,6 @@
+#ifndef __IRIDIUM_CODEGEN_H__
+#define __IRIDIUM_CODEGEN_H__
+
+// Code Generator Driver
+
+#endif
---
+++ rebuild.sh
@@ -0,0 +1,3 @@
+mkdir build 
+cd build
+cmake ../ -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DLLVM_DIR=~/llvm/llvm15-0-6/lib/cmake/llvm
