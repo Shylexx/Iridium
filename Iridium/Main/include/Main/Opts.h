58909cf8f5026b1ba448e2f96fc7b9ad429e67fd
Alex Dauncey <alexanderdauncey@gmail.com>
2023-03-30 15:06:41 +0100

    cli arg testing

---
+++ Iridium/Main/include/Main/Opts.h
@@ -0,0 +1,11 @@
+#ifndef __IRIDIUM_CLI_OPTS_H__
+#define __IRIDIUM_CLI_OPTS_H__
+
+#include <string>
+
+struct Opts {
+  bool printLLVMIR;
+  std::string sLibPath;
+};
+
+#endif
--- Iridium/Main/src/Main/IridiumEntry.cpp
+++ Iridium/Main/src/Main/IridiumEntry.cpp
@@ -1,7 +1,25 @@
 #include "Main/IridiumDriver.h"
 #include <iostream>
+#include <cxxopts.hpp>
 
 int main(int argc, char*argv[]) {
+  cxxopts::Options cli_opts("iridiumc", "Iridium Compiler");
+
+  // temporarily using hardcoded defaut path
+  cli_opts.add_options()
+    ("h,help", "Print Usage Help")
+    ("print-ir", "Print LLVM IR", cxxopts::value<bool>()->default_value("false"))
+    ("f, file", "Entry point file to compile", cxxopts::value<std::string>())
+    ("slib-path", "Standard library path, if not default (XDG_DATA)", cxxopts::value<std::string>()->default_value("/home/alex/.iridium"))
+    ;
+
+  auto opts = cli_opts.parse(argc,argv);
+
+  if(opts.count("help")) {
+    std::cout << cli_opts.help() << std::endl;
+    return 0;
+  }
+
   iridium::Driver driver;
   std::cerr << "Num of Args: " << argc << std::endl;
   if(argc > 1) {
